234cb35a2f3f2e6cdb5b12bec1b92868d38d6fc9
Arnav Dixit <arnavdixit022611@gmail.com>
2026-06-05 16:03:59 +0530

    reading through docs and working through them.

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
-import { Client, GatewayIntentBits } from 'discord.js';
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { token } = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
-    ],
-    presence: { status: 'online' }
+    intents: [GatewayIntentBits.Guilds]
 });
 
-
-
-client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
-    
-    if (message.content.toLowerCase() === 'whats up') {
-        try {
-            await message.author.send(`Echo ${message.content}`);
-        } catch (error) {
-            console.error(`Could not DM user: ${error.message}`);
-        }
-    }
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+client.login(process.env.DISCORD_TOKEN);
 
-    
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
