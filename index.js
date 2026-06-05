d3b9c2fd77df538ab6bd5f2be824ac96ab8c2034
Arnav Dixit <arnavdixit022611@gmail.com>
2026-06-05 05:59:31 +0530

    THE BIRTH OF BANANA LECLERC

---
+++ index.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+
+});
